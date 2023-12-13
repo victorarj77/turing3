8b907f7fdbf834ff11bdbb839a6701847b59229f
Victor Araujo <nvictor18g@gmail.com>
2023-12-13 13:31:53 -0300

    Corrigir configuração de porta para o Heroku

--- turing3.py
+++ turing3.py
@@ -1,4 +1,5 @@
 import logging
+import re
 import requests
 from telegram.ext import Updater, MessageHandler, Filters
 
@@ -7,32 +8,39 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 # Função para obter a URL de redirecionamento a partir de um link encurtado
-def obter_url_redirecionamento(link_encurtado):
+def obter_url_redirecionamento(message_text):
     try:
-        logger.info(f"Tentando desencurtar: {link_encurtado}")
+        logger.info("Tentando encontrar link na mensagem")
 
-        # Realizar uma requisição HTTP GET e seguir os redirecionamentos
-        response = requests.get(link_encurtado)
-        url_redirecionamento = response.url
+        # Utilizar expressão regular para encontrar o link na mensagem
+        match = re.search(r'https?://[^\s]+', message_text)
+        
+        if match:
+            link_encurtado = match.group(0)
+            logger.info(f"Link encontrado na mensagem: {link_encurtado}")
 
-        logger.info(f"URL desencurtada: {url_redirecionamento}")
+            # Realizar uma requisição HTTP GET e seguir os redirecionamentos
+            response = requests.get(link_encurtado)
+            url_redirecionamento = response.url
 
-        return url_redirecionamento
+            logger.info(f"URL desencurtada: {url_redirecionamento}")
+
+            return url_redirecionamento
 
     except Exception as e:
         logger.error(f"Erro ao obter URL de redirecionamento: {e}")
-        return link_encurtado
+
+    return None
 
 # Manipulador de mensagens
 def handle_messages(update, context):
     message_text = update.message.text
-    
-    if 'http' in message_text:
-        url_redirecionamento = obter_url_redirecionamento(message_text)
-        if url_redirecionamento:
-            context.bot.send_message(chat_id=update.message.chat_id, text=f"⚽  ULTRON - LINK DA PARTIDA123:  ⚽\n{url_redirecionamento}")
-        else:
-            context.bot.send_message(chat_id=update.message.chat_id, text="Erro ao obter URL de redirecionamento.")
+
+    url_redirecionamento = obter_url_redirecionamento(message_text)
+    if url_redirecionamento:
+        context.bot.send_message(chat_id=update.message.chat_id, text=f"LINK: \n{url_redirecionamento}")
+    else:
+        context.bot.send_message(chat_id=update.message.chat_id, text="Erro ao obter URL de redirecionamento.")
 
 # Substitua 'SEU_TOKEN' pelo token fornecido pelo BotFather
 updater = Updater(token='6854755484:AAG-jgENE7UorXuH9I_UdxyttivBQrncG20', use_context=True)
