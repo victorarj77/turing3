aef111ae7acb92bec0ee1af0bac2d76f8ad92821
victorarj77 <153679462+victorarj77@users.noreply.github.com>
2023-12-12 21:50:26 -0300

    Update turing3.py

--- turing3.py
+++ turing3.py
@@ -1,7 +1,8 @@
 import logging
 import re
-from telegram.ext import Updater, MessageHandler, CallbackContext
 import requests
+from telegram.ext import Updater, MessageHandler, CallbackContext
+from urllib.parse import unquote
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -15,14 +16,23 @@ def obter_url_redirecionamento(link_encurtado):
         logger.error(f"Erro ao obter URL de redirecionamento: {e}")
         return link_encurtado
 
+def desencurtar_link(link_encurtado):
+    try:
+        desencurtado = unquote(link_encurtado)
+        return desencurtado
+    except Exception as e:
+        logger.error(f"Erro ao desencurtar link: {e}")
+        return link_encurtado
+
 def handle_messages(update, context):
     if update.message and update.message.text:
         texto_original = update.message.text
         links_encurtados = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', texto_original)
         for link_encurtado in links_encurtados:
             url_redirecionamento = obter_url_redirecionamento(link_encurtado)
+            link_desencurtado = desencurtar_link(url_redirecionamento)
             mensagem_personalizada = "LINK DA PARTIDA:"
-            texto_original = texto_original.replace(link_encurtado, f"{mensagem_personalizada}\n{url_redirecionamento}")
+            texto_original = texto_original.replace(link_encurtado, f"{mensagem_personalizada}\n{link_desencurtado}")
         context.bot.send_message(chat_id=update.message.chat_id, text=texto_original)
 
 def main():
