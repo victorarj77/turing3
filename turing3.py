da005e9659d689a75fb3b96b6cd76283b5ea449e
Victor Araujo <nvictor18g@gmail.com>
2023-12-13 13:36:42 -0300

    87

--- turing3.py
+++ turing3.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 # Função para obter a URL de redirecionamento a partir de um link encurtado
 def obter_url_redirecionamento(message_text):
     try:
-        logger.info("Tentando encontrar link na mensagem")
+        logger.info("Tentando encontrar e substituir link na mensagem")
 
         # Utilizar expressão regular para encontrar o link na mensagem
         match = re.search(r'https?://[^\s]+', message_text)
@@ -25,22 +25,21 @@ def obter_url_redirecionamento(message_text):
 
             logger.info(f"URL desencurtada: {url_redirecionamento}")
 
-            return url_redirecionamento
+            # Substituir o link encurtado pelo desencurtado na mensagem
+            nova_mensagem = message_text.replace(link_encurtado, url_redirecionamento)
+            return nova_mensagem
 
     except Exception as e:
         logger.error(f"Erro ao obter URL de redirecionamento: {e}")
 
-    return None
+    return message_text
 
 # Manipulador de mensagens
 def handle_messages(update, context):
     message_text = update.message.text
 
-    url_redirecionamento = obter_url_redirecionamento(message_text)
-    if url_redirecionamento:
-        context.bot.send_message(chat_id=update.message.chat_id, text=f"LINK: \n{url_redirecionamento}")
-    else:
-        context.bot.send_message(chat_id=update.message.chat_id, text="Erro ao obter URL de redirecionamento.")
+    nova_mensagem = obter_url_redirecionamento(message_text)
+    context.bot.send_message(chat_id=update.message.chat_id, text=nova_mensagem)
 
 # Substitua 'SEU_TOKEN' pelo token fornecido pelo BotFather
 updater = Updater(token='6854755484:AAG-jgENE7UorXuH9I_UdxyttivBQrncG20', use_context=True)
