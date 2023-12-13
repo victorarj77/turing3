642f8f20be9c70b47f44b29bc46334ee5a42530d
victorarj77 <153679462+victorarj77@users.noreply.github.com>
2023-12-13 11:08:13 -0300

    Update turing3.py

--- turing3.py
+++ turing3.py
@@ -1,51 +1,48 @@
 import logging
-import re
-import os
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from telegram.ext import Updater, MessageHandler, CallbackContext
+import requests
+from telegram.ext import Updater, MessageHandler, Filters
 
+# Configurar o logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Configurações do ChromeDriver para o Heroku
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-
-# Configuração do ChromeDriver com path local no seu repositório
-chrome_bin = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.binary_location = chrome_bin
-driver = webdriver.Chrome(options=chrome_options)
-
-
+# Função para obter a URL de redirecionamento a partir de um link encurtado
 def obter_url_redirecionamento(link_encurtado):
     try:
-        driver.get(link_encurtado)
-        url_redirecionamento = driver.current_url
+        logger.info(f"Tentando desencurtar: {link_encurtado}")
+
+        # Realizar uma requisição HTTP GET e seguir os redirecionamentos
+        response = requests.get(link_encurtado)
+        url_redirecionamento = response.url
+
+        logger.info(f"URL desencurtada: {url_redirecionamento}")
+
         return url_redirecionamento
+
     except Exception as e:
         logger.error(f"Erro ao obter URL de redirecionamento: {e}")
         return link_encurtado
 
+# Manipulador de mensagens
 def handle_messages(update, context):
-    if update.message and update.message.text:
-        texto_original = update.message.text
-        links_encurtados = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', texto_original)
-        for link_encurtado in links_encurtados:
-            url_redirecionamento = obter_url_redirecionamento(link_encurtado)
-            mensagem_personalizada = "LINK DA PARTIDA:"
-            texto_original = texto_original.replace(link_encurtado, f"{mensagem_personalizada}\n{url_redirecionamento}")
-        context.bot.send_message(chat_id=update.message.chat_id, text=texto_original)
+    message_text = update.message.text
+    
+    if 'http' in message_text:
+        url_redirecionamento = obter_url_redirecionamento(message_text)
+        if url_redirecionamento:
+            context.bot.send_message(chat_id=update.message.chat_id, text=f"⚽  ULTRON - LINK DA PARTIDA:  ⚽\n{url_redirecionamento}")
+        else:
+            context.bot.send_message(chat_id=update.message.chat_id, text="Erro ao obter URL de redirecionamento.")
 
-def main():
-    updater = Updater(token='6854755484:AAG-jgENE7UorXuH9I_UdxyttivBQrncG20', use_context=True)
-    dp = updater.dispatcher
-    dp.add_handler(MessageHandler(callback=handle_messages, filters=None))
-    updater.start_polling()
-    updater.idle()
+# Substitua 'SEU_TOKEN' pelo token fornecido pelo BotFather
+updater = Updater(token='6854755484:AAG-jgENE7UorXuH9I_UdxyttivBQrncG20', use_context=True)
+dp = updater.dispatcher
 
-if __name__ == '__main__':
-    main()
+# Adicionar um manipulador de mensagens ao dispatcher
+dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_messages))
+
+# Iniciar o bot
+updater.start_polling()
+
+# Aguardar o bot ser encerrado manualmente ou por algum erro
+updater.idle()
