9d4a2d66cf37b9165a348507d1b91ef37319e906
victorarj77 <153679462+victorarj77@users.noreply.github.com>
2023-12-12 21:41:22 -0300

    Update turing3.py

--- turing3.py
+++ turing3.py
@@ -18,17 +18,21 @@ def obter_url_redirecionamento(link_encurtado):
 def handle_messages(update, context):
     if update.message and update.message.text:
         texto_original = update.message.text
+        logger.info(f"Texto Original: {texto_original}")
         links_encurtados = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', texto_original)
+        logger.info(f"Links Enviados: {links_encurtados}")
         for link_encurtado in links_encurtados:
             url_redirecionamento = obter_url_redirecionamento(link_encurtado)
             mensagem_personalizada = f"LINK DA PARTIDA: {url_redirecionamento}"
             texto_original = texto_original.replace(link_encurtado, mensagem_personalizada)
+        logger.info(f"Texto Modificado: {texto_original}")
         context.bot.send_message(chat_id=update.message.chat_id, text=texto_original)
 
 def main():
     updater = Updater(token='6854755484:AAG-jgENE7UorXuH9I_UdxyttivBQrncG20', use_context=True)
     dp = updater.dispatcher
     dp.add_handler(MessageHandler(Filters.text & ~Filters.command, callback=handle_messages))
+    
     updater.start_polling()
     updater.idle()
 
